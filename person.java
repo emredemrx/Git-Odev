8f1b402af2f9a88627036c8a5afe0aa22205d1d5
emred <femredemir699@gmail.com>
2024-05-18 18:01:33 +0300

    json, xml ve yaml dosyaları eklendi

---
+++ person.java
@@ -0,0 +1,20 @@
+public class person {
+    int personelNo;
+    String name;
+    String lastname;
+    String gender;
+    String tcNumber;
+    int age;
+
+    String email;
+    String gsm;
+    String address;
+
+    String title;
+    String department;
+    String location;
+    int startingdate;
+    String status;
+    String managerName;
+    double wage;
+}
